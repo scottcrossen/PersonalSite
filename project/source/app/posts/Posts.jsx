320401dba3b117524f0e28cef531268b923b3485
Scott Leland Crossen <scottcrossen42@gmail.com>
2017-10-20 10:53:22 -0600

    working on post page

--- project/source/app/posts/Posts.jsx
+++ project/source/app/posts/Posts.jsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import {Carousel} from 'react-responsive-carousel';
 import {FatBorder} from 'app/util/FatBorder.jsx'
-import {Image, FitTypes} from 'app/util/Image'
 import {FatBreak} from 'app/util/FatBreak.jsx'
+import {Image, FitTypes} from 'app/util/Image'
+import classnames from 'classnames'
+import React from 'react'
+
+import styles_1 from 'styles/posts/posts.scss';
+import styles_2 from 'react-responsive-carousel/lib/styles/carousel.min.css';
 
 type Props = {
 }
@@ -14,17 +19,43 @@ type State = {
 export class Posts extends React.Component<Props, State>{
 
   render = (): React$Element<*> => {
-    const {height} = this.state
+    const {height, width} = this.state
     const {children} = this.props
     const imageHeight = height * 0.3
+    // TODO: fix the carousel width for small displays
+    // TODO: make the overlay text not black
+    const carouselBorder = Math.min(width, height) * 0.4
     return (
-      <FatBorder>
-        <div style={{height: imageHeight}}>
-          <Image source={require('assets/images/global/y_mount_top.jpg')}
-            fit={FitTypes.none} />
+      <div className={classnames('posts')}>
+        <div className={classnames('overlay')}>
+          <FatBorder>
+            <h1>Blog Posts</h1>
+          </FatBorder>
         </div>
-        <FatBreak />
-      </FatBorder>
+        <FatBorder>
+          <div style={{height: imageHeight}}>
+            <Image source={require('assets/images/global/y_mount_top.jpg')}
+              fit={FitTypes.none}></Image>
+          </div>
+          <FatBreak />
+        </FatBorder>
+          <div style={{width: width - 2 * carouselBorder, marginLeft: carouselBorder, marginRight: carouselBorder}}>
+          <Carousel showStatus={false} showThumbs={false}>
+            <div>
+                <img src='assets/images/global/y_mount_top.jpg' />
+                <p className={classnames('post-1')}>Legend 1</p>
+            </div>
+            <div>
+                <img src='assets/images/global/y_mount_top.jpg' />
+                <p className={classnames('post-2')}>Legend 2</p>
+            </div>
+            <div>
+                <img src='assets/images/global/y_mount_top.jpg' />
+                <p className={classnames('post-3')}>Legend 3</p>
+            </div>
+          </Carousel>
+        </div>
+      </div>
     )
     // Need title on page
     // https://codepen.io/leandrowd/pen/xLvOPd
